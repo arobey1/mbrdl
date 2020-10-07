163fe70a60efc7deccdf5f8b65c6ebc7ff44da87
Alex Robey <arobey1@seas.upenn.edu>
2020-10-07 13:52:33 -0400

    added print statements to match dataloaders.py

--- core/data/munit_loaders.py
+++ core/data/munit_loaders.py
@@ -16,13 +16,16 @@ def get_munit_loaders(args):
         return get_svhn_loaders(args)
     elif args.dataset == 'gtsrb':
         return get_gtsrb_loaders(args)
-    elif args.dataset == 'cure_tsr':
+    elif args.dataset == 'cure-tsr' or args.dataset == 'cure_tsr':
         return get_cure_tsr_loaders(args)
     else:
         raise NotImplementedError(f'Dataset {args.dataset} is not implemented.')
 
 def get_svhn_loaders(args):
     """Get loaders with SVHN data for training MUNIT."""
+
+    if args.local_rank == 0:
+        print(f'Loading SVHN {args.source_of_nat_var} dataset...')
 
     train_A = SVHNSubsets('train', args.source_of_nat_var, dom='low', 
                                     args=args, return_labels=False)
@@ -38,6 +41,9 @@ def get_svhn_loaders(args):
 def get_gtsrb_loaders(args):
     """Get loaders with GTSRB data for training MUNIT."""
 
+    if args.local_rank == 0:
+        print(f'Loading GTSRB {args.source_of_nat_var} dataset...')
+
     train_A = GTSRBSubsets('train', args.source_of_nat_var, dom='low', 
                             args=args, return_labels=False)
     train_B = GTSRBSubsets('train', args.source_of_nat_var, dom='high',
@@ -52,16 +58,19 @@ def get_gtsrb_loaders(args):
 def get_cure_tsr_loaders(args):
     """Get loaders with CURE-TSR data for training MUNIT."""
 
+    if args.local_rank == 0:
+        print(f'Loading CURE-TSR {args.source_of_nat_var} dataset...')
+
     train_A = CUREDataset('train', args.source_of_nat_var, level=0,
                             args=args, return_labels=False)
     train_B = CUREDataset('train', args.source_of_nat_var, level=5,
                             args=args, return_labels=False)
     test_A = CUREDataset('test', args.source_of_nat_var, level=0,
                             args=args, return_labels=False)
-    train_B = CUREDataset('test', args.source_of_nat_var, level=5,
+    test_B = CUREDataset('test', args.source_of_nat_var, level=5,
                             args=args, return_labels=False)
 
-    return _to_loader(train_A)
+    return _to_loader(train_A, train_B, test_A, test_B)
 
 def _to_loader(*args):
     """Turn datasets into dataloaders for MUNIT."""
