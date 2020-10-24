967a52ad2b31c9ecdb56a71b00cb7d3140cc3bfe
Alex Robey <arobey1@seas.upenn.edu>
2020-10-24 17:44:06 -0400

    updated for MNIST

--- train_munit.sh
+++ train_munit.sh
@@ -4,15 +4,15 @@
 # SVHN, GTSRB, and CURE-TSR datasets.
 
 # Variables needed to select dataset and source of natural variation
-export DATASET='cure-tsr'
-export DATA_DIR=./datasets/cure_tsr/raw_data
-export SOURCE='snow'
+export DATASET='mnist'
+export DATA_DIR=./datasets/mnist
+export SOURCE='background-color'
 export SZ=32
 
 # Path to MUNIT configuration file.  Edit this file to change the number of iterations, 
 # how frequently checkpoints are saved, and other properties of MUNIT.
 # The parameter `style_dim` corresponds to the dimension of `delta` in our work.
-export CONFIG_PATH=core/models/munit/munit.yaml
+export CONFIG_PATH=core/models/munit/tiny_munit.yaml
 
 # Output images and checkpoints will be saved to this path.
 export OUTPUT_PATH=core/models/munit/results
